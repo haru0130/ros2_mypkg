5e9416adc1da6ab200d4855a137637769167abd6
Haruki Matsukawa <sqwk7759@gmail.com>
2022-12-31 23:01:43 +0900

    fix

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -5,15 +5,18 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-
-    talker = launch_ros.action.Node(
+    
+    talker = launch_ros.actions.Node(
         package='mypkg',
         executable='talker',
     )
-    listener = launch_ros.action.Node(
+    listener = launch_ros.actions.Node(
         package='mypkg',
         executable='listener',
         output='screen'
     )
 
     return launch.LaunchDescription([talker, listener])
+
+
+
