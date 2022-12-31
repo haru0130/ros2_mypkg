ff16c16b4142765b10d5ff639f0698edb599d20f
Haruki Matsukawa <sqwk7759@gmail.com>
2022-12-31 19:29:14 +0900

    fix

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,21 +1,20 @@
 import rclpy                     #ROS2のクライアントのためのライブラリ
 from rclpy.node import Node      #ノードを実装するためのNodeクラス（クラスは第10回で）
-from person_msgs.msg import Person 
+from std_msgs.msg import Int16 
 
 class Talker():
-    def__init__(self):
+    def __init__(self):
         self.pub = node.create_publisher(Int16, "count_up", 10)
         self.n = 0
 
 rclpy.init()
 node = Node("talker")            #ノード作成（nodeという「オブジェクト」を作成）
-pub = node.create_publisher(Person, "Person", 10)   #パブリッシャのオブジェクト作成
-n = 0 #カウント用変数
+talker = Talker()
 
-def cb():          #17行目で定期実行されるコールバック関数
+def cb():
     msg = Int16()
     msg.data = talker.n
-    talker.pub.publish(msg)       
+    talker.pub.publish(msg)
     talker.n += 1
     
 node.create_timer(0.5, cb)  #タイマー設定
