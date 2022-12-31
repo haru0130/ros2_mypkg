0f114a691c909461c814ef7e8b44ce430026f556
Haruki Matsukawa <sqwk7759@gmail.com>
2022-12-31 23:33:05 +0900

    fix

--- install/mypkg/share/mypkg/talk_listen.launch.py
+++ install/mypkg/share/mypkg/talk_listen.launch.py
@@ -1,19 +1,19 @@
 import launch
 import launch.actions
-import launch.substitution
-
+import launch.substitutions
+import launch_ros.actions
 
 
 def generate_launch_description():
 
-    talker = launch_ros.action.Node(
-        package='mypkg',
-        executable='talker',
-    )
-    listener = launch_ros.action.Node(
-        package='mypkg'
-        executable='listener',
-        output='screen'
-    )
+     talker = launch_ros.actions.Node(
+         package='mypkg',
+         executable='talker',
+         )
+     listener = launch_ros.actions.Node(
+         package='mypkg',
+         executable='listener',
+         output='screen'
+         )
 
-    return launch.LaunchDescription([talker, listener])
+     return launch.LaunchDescription([talker, listener])
