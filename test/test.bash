6373627b13437b60000c440a324313399b653c9b
Haruki Matsukawa <sqwk7759@gmail.com>
2023-01-03 02:29:36 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -3,10 +3,12 @@
 dir=~
  [ "$1" != "" ] && dir="$1"   
 
-cd $dir/ros2_ws/src/mypkg
+cd $dir/ros2_ws #/src/mypkg
 colcon build
+
 source $dir/.bashrc
+
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+#cat /tmp/mypkg.log |
+#grep 'Listen: 10'
